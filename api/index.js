cc6a65f134cd536412eef2b1220bc6d7e2c5d654
MaazMuhammad2 <maaz86507@gmail.com>
2024-08-30 20:05:54 +0500

    connet to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(300, ()=>{
-    console.log("Server is running on port 300!")
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(300, () => {
+  console.log("Server is running on port 300!");
+});
