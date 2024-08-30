3097f316d603bc4871ee483ac3d20a0e7d440dcf
MaazMuhammad2 <maaz86507@gmail.com>
2024-08-30 21:28:12 +0500

    created a user model

---
+++ api/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      //   lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      required: [true, "Email is required"],
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary string
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
