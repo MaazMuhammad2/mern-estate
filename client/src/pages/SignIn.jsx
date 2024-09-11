d60c6ac38c6513e19643b4f4cac5e9fb82119fe5
MaazMuhammad2 <maaz86507@gmail.com>
2024-09-12 00:41:16 +0500

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signinStart,
+  signinSuccess,
+  signinFailure,
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -20,7 +28,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signinStart());
 
       const res = await fetch("/api/auth/signin", {
         method: "POST",
@@ -34,17 +42,14 @@ export default function SignIn() {
       console.log("data is: ", data);
 
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signinFailure(data.message));
         return;
       }
 
-      setLoading(false);
-      setError(null);
+      dispatch(signinSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(fasle);
-      setError(error.message);
+      dispatch(signinFailure(data.message));
     }
   };
 
@@ -71,7 +76,7 @@ export default function SignIn() {
             disabled={loading}
             className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:poacity-80"
           >
-            {loading ? "LOADING.." : "Sign up"}
+            {loading ? "LOADING.." : "Sign in"}
           </button>
         </form>
         <div className="flex gap-2 mt-5">
