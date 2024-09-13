1d866ae46126947216601731a5e36c6a03c687c2
MaazMuhammad2 <maaz86507@gmail.com>
2024-09-13 23:39:14 +0500

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,17 +1,94 @@
 import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  // console.log(file);
+  console.log("this is the filePerc", filePerc);
+  console.log("this is the form data", formData);
+  console.log(fileUploadError);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file); //Resumable: The "resumable" part means that if the network connection is interrupted during the upload, Firebase can resume the upload once the connection is restored, without starting over from the beginning.
+
+    // state_changed:>> This is an event that Firebase triggers whenever the state of the file upload changes.
+    // The snapshot is an object that contains the current state of the upload at the moment this event (state_changed) is triggered
+
+    uploadTask.on("state_changed",
+      (snapshot) => {
+        // Handle progress
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        // Handle error
+        setFileUploadError(true);
+      },
+      () => {
+        // Handle successful completion
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+   
+  };
+  console.log("this is another form Data", formData);
 
   return (
     <div className="p-3 max-w-xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col  gap-4" action="">
+        <input
+          type="file"
+          onChange={(e) => setFile(e.target.files[0])}
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
+
         <img
           className="rounded-full h-24 mt-3 w-24 object-cover cursor-pointer self-center"
-          src={currentUser?.avatar}
+          src={formData.avatar || currentUser?.avatar}
           alt="profile"
+          onClick={() => fileRef.current.click()}
         />
+
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">
+              Error image upload(image must be less than 2mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className="text-green-700">File uploaded successfully!</span>
+          ) : (
+            ""
+          )}
+        </p>
+
         <input
           type="text"
           placeholder="username"
@@ -35,11 +112,10 @@ export default function Profile() {
         </button>
       </form>
 
-<div className="flex justify-between mt-5">
-  <span className="text-red-700 cursor-pointer">Delete Account</span>
-  <span className="text-green-700 cursor-pointer">Sign out</span>
-</div>
-
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-green-700 cursor-pointer">Sign out</span>
+      </div>
     </div>
   );
 }
