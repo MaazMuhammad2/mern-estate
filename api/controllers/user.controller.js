00679505d1e566ddc30c1844d5a87ce31a7b5637
MaazMuhammad2 <maaz86507@gmail.com>
2024-09-24 23:18:45 +0500

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,42 +2,52 @@ import { User } from "../models/user.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import bcryptjs from "bcryptjs";
+import Listing from "../models/listing.models.js";
 
-    const updateUser = asyncHandler(async (req,res,next) => {
-    if (req.user?.id !== req.params.id) {
-        throw new ApiError(401, "You can only update your own account");
+const updateUser = asyncHandler(async (req, res, next) => {
+  if (req.user?.id !== req.params.id) {
+    throw new ApiError(401, "You can only update your own account");
+  }
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
 
-    try {
-        if (req.body.password) {
-        req.body.password = bcryptjs.hashSync(req.body.password, 10);
-        }
-
-        const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set: {
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            avatar: req.body.avatar,
-            },
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
         },
-        { new: true }
-        ).select("-password");
+      },
+      { new: true }
+    ).select("-password");
 
-        res.status(200).json(updatedUser);
-    } catch (error) {
-        throw new ApiError(error);
-    }
-    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    throw new ApiError(error);
+  }
+});
 
-    const deleteUser = asyncHandler(async(req, res)=>{
-        if (req.user.id !== req.params.id) {
-            throw new ApiError(401, "you can only delete your own account!")
-        }
-        await User.findByIdAndDelete(req.params.id)
-        res.clearCookie('access_token')
-        res.status(200).json("User has been deleted!")
-    })
-export { updateUser, deleteUser };
+const deleteUser = asyncHandler(async (req, res) => {
+  if (req.user.id !== req.params.id) {
+    throw new ApiError(401, "you can only delete your own account!");
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.clearCookie("access_token");
+  res.status(200).json("User has been deleted!");
+});
+
+const getUserListings = asyncHandler(async (req, res) => {
+  if (req.user.id === req.params.id) {
+    const lisitings = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(lisitings);
+  } else {
+    throw new ApiError(401, "You can only view your own lisitngs");
+  }
+});
+export { updateUser, deleteUser, getUserListings };
