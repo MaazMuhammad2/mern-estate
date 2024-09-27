575ac3d022631ca3815a2d324c6fe3343381c7cd
MaazMuhammad2 <maaz86507@gmail.com>
2024-09-27 16:41:48 +0500

    add image slider to the listing page

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
+export default function Listing() {
+  SwiperCore.use([Navigation]);
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchListingData = async () => {
+      try {
+        setError(false);
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListingData();
+  }, [params.listingId]);
+
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl"> Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong</p>
+      )}
+      {listing && !error && !loading && (
+      <>
+      <div className="w-full">
+        <Swiper navigation>
+          {listing.imageUrls.map((url) => (
+            <SwiperSlide key={url}>
+              <div
+                className="h-[400px] w-full"
+                style={{
+                  background: `url(${url}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+    
+      )}
+    </main>
+  );
+}
