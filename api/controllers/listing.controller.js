17e96428d61cb07ce53d9f7a22c69920c9a6f3cc
MaazMuhammad2 <maaz86507@gmail.com>
2024-10-01 17:25:31 +0500

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -34,20 +34,72 @@ const updateListing = asyncHandler(async (req, res) => {
   }
 
   try {
-    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
     res.status(200).json(updatedListing);
-} catch (error) {
+  } catch (error) {
     console.error("Error updating listing:", error);
     res.status(500).json({ message: "Internal Server Error" });
-}
+  }
 });
 
-const getListing = asyncHandler(async(req,res)=>{
+const getListing = asyncHandler(async (req, res) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
-    throw new ApiError(404, 'Listing not found!')
+    throw new ApiError(404, "Listing not found!");
   }
-  res.status(200).json(listing)
-})
+  res.status(200).json(listing);
+});
 
-export { createListing, deleteListing, updateListing, getListing };
+const getListings = asyncHandler(async (req, res) => {
+  const limit = parseInt(req.query.limit) || 9;
+  const startIndex = parseInt(req.query.startIndex) || 0;
+
+  let offer = req.query.offer;
+  if (offer === undefined || offer === false) {
+    offer = {
+      $in: [false, true],
+    };
+  }
+
+  let furnished = req.query.furnished;
+  if (furnished === undefined || furnished === false) {
+    furnished = {
+      $in: [true, false],
+    };
+  }
+
+  let parking = req.query.parking;
+  if (parking === undefined || parking === false) {
+    parking = { $in: [true, false] };
+  }
+
+  let type = req.query.type;
+  if (type === undefined || type === "all") {
+    type = { $in: ["sale", "rent"] };
+  }
+
+  const searchTerm = req.query.searchTerm || "";
+  const sort = req.query.sort || "createdAt";
+  const order = req.query.order || "desc";
+
+  const listings = await Listing.find({
+    name: { $regex: searchTerm, $options: "i" },
+    offer,
+    furnished,
+    parking,
+    type,
+  })
+    .sort({
+      [sort]: order,
+    })
+    .limit(limit)
+    .skip(startIndex);
+
+  return res.status(200).json(listings);
+});
+
+export { createListing, deleteListing, updateListing, getListing, getListings };
