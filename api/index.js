933d4d60f4f19454a4a2e8de122aa261cd518b1c
MaazMuhammad2 <maaz86507@gmail.com>
2024-10-05 22:33:46 +0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,9 +5,12 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 // Add middleware to parse JSON
 app.use(express.json());
@@ -32,6 +35,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
