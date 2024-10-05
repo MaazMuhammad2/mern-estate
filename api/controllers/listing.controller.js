9c226892c71c1a0f3e2976bd0cdafbd9def984cb
MaazMuhammad2 <maaz86507@gmail.com>
2024-10-05 22:16:22 +0500

    complete home page

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -54,62 +54,56 @@ const getListing = asyncHandler(async (req, res) => {
   res.status(200).json(listing);
 });
 
-const getListings = asyncHandler(async (req, res) => {
-  const limit = parseInt(req.query.limit) || 9;
-  const startIndex = parseInt(req.query.startIndex) || 0;
-
-  // Convert string "true"/"false" to boolean values
-  let offer = req.query.offer === "true";
-  if (req.query.offer === undefined) {
-    offer = { $in: [true, false] };
-  }
-
-  let furnished = req.query.furnished === "true";
-  if (req.query.furnished === undefined) {
-    furnished = { $in: [true, false] };
-  }
-
-  let parking = req.query.parking === "true";
-  if (req.query.parking === undefined) {
-    parking = { $in: [true, false] };
-  }
-
-  let type = req.query.type;
-  if (type === undefined || type === "all") {
-    type = { $in: ["sale", "rent"] };
-  }
-
-  const searchTerm = req.query.searchTerm || "";
-  const sort = req.query.sort || "createdAt";
-  
-  // Ensure order is either "asc" or "desc"
-  const order = req.query.order === "asc" || req.query.order === "desc" ? req.query.order : "desc";
-  const nameFilter = searchTerm ? { name: { $regex: searchTerm, $options: "i" } } : {};
-
-
+const getListings = async (req, res) => {
   try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
     const listings = await Listing.find({
-      nameFilter,
+      name: { $regex: searchTerm, $options: 'i' },
       offer,
       furnished,
       parking,
       type,
     })
-      .sort({
-        [sort]: order, // Dynamically apply sorting field and order
-      })
+      .sort({ [sort]: order })
       .limit(limit)
       .skip(startIndex);
 
     return res.status(200).json(listings);
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: "Server Error",
-      error: error.message,
-    });
+    throw new ApiError(400, error)
   }
-});
+};
 
 
 export { createListing, deleteListing, updateListing, getListing, getListings };
